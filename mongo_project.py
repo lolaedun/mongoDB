b8f31fb3e23d2f9c1e8bf0005723d2f86f8b06a8
Lola <mledun@hotmail.com>
2021-08-13 19:15:14 +0000

    initial commit

---
+++ mongo_project.py
@@ -0,0 +1,18 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "celebrities"
+
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB: %s") % e
+
+
